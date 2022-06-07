bc60eb2edc70851cc1f2ad940aab9acd67af234e
KhangTA <truongankhang@gmail.com>
2022-06-07 21:57:52 +0700

    first commit

---
+++ src/main/java/App.java
@@ -0,0 +1,8 @@
+
+public class App {
+    public static void main(String[] args) {
+        Example example = new Example();
+        int sum = example.sum(10, 20);
+        System.out.print(sum);
+    }
+}
